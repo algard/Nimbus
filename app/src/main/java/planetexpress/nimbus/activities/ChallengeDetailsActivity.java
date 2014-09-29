2cbd10d55cf64f6546db790d5586a53d51546857
Lia Zadoyan <liazadoyan@gmail.com>
2014-09-29 11:48:24 -0700

    butterknife and add clients button temp

--- app/src/main/java/planetexpress/nimbus/activities/ChallengeDetailsActivity.java
+++ app/src/main/java/planetexpress/nimbus/activities/ChallengeDetailsActivity.java
@@ -5,19 +5,30 @@ import android.graphics.Outline;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import planetexpress.nimbus.R;
 
 /**
  * Created by Lia.Zadoyan on 9/29/2014.
  */
 public class ChallengeDetailsActivity extends Activity {
+    @InjectView(R.id.addClientsButton) protected Button addClientsButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge_details);
+        ButterKnife.inject(this);
         setTitle("Challenge Details");
 
-
+        addClientsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "AddClients", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
